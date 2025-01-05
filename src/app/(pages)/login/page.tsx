e242aa8d3646d35846bccdbfd87b698793885853
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-01-05 19:52:38 +0300

    front01

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const LoginPage = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <p>Login Sayfasıdır...</p>
+      <Button>Naberrr</Button>
+    </div>
+  );
+};
+
+export default LoginPage;
